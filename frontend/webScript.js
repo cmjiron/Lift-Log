f56a2d4614207aa031b5985d4c0667ff2fb1eb56
cmjiron <c.m.jiron@comcast.net>
2023-09-01 16:07:51 -0400

    Adding backend and frontend seperation

---
+++ frontend/webScript.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Get the form element
+  var form = document.getElementById('dayForm');
+
+  // Get the textarea element
+  var messageInput = form.querySelector('#day');
+
+  // Set the default text
+  var defaultText = "Day 1";
+  messageInput.value = defaultText;
+
+  // Add an event listener for form submission
+  form.addEventListener('submit', function (event) {
+    // If the user hasn't changed the text, use the default text as the submitted value
+    if (messageInput.value === defaultText) {
+      messageInput.value = defaultText;
+    }
+    // Otherwise, the user's input will be submitted
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the button, form template, and the ul element
+  const addItemButton = document.getElementById('createNewWorkoutButton');
+  const formTemplate = document.getElementById('exerciseTemplate');
+  const itemList = document.getElementById('exerciseList');
+
+  // Add a click event listener to the button
+  addItemButton.addEventListener('click', function () {
+    // Clone the form template
+    const clone = formTemplate.cloneNode(true);
+
+    // Remove the 'display: none;' style to make it visible
+    clone.style.display = 'block'; // or 'block', 'inline', etc., based on your needs
+
+    // Append the clone to the ul element
+    itemList.appendChild(clone);
+  });
+});
